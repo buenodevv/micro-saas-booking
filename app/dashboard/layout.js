24478a1dd83360f97d31bd75f55935928af4e85e
Rodrigo Bueno <buenodevv@gmail.com>
2024-04-17 06:14:23 -0300

    feat: add logo and add image logo

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,5 +1,5 @@
-import Header from "./components/Header";
-import SideBar from "./components/SideBar";
+import Header from "./_components/Header";
+import SideBar from "./_components/SideBar";
 
 
 export default function DashboardLayout({
