400bb3ed37c2e97a2bed15e46860b7d32010d2cc
Rodrigo Bueno <buenodevv@gmail.com>
2024-05-06 06:31:26 -0300

    feat: page index

--- app/page.js
+++ app/page.js
@@ -1,9 +1,15 @@
-import Dashboard from "./dashboard/page";
+'use client';
+import Link from "next/link";
+import Login from "./login/page";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  const { isAuthenticated } = getKindeServerSession();
   return (
     <div>
-      <Dashboard />
+      {isAuthenticated() && redirect("/dashboard")}
+      <Login />
     </div>
   );
 }
